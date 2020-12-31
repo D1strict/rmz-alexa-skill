a53993f8db381042729d8e09fdc6eb6769884acd
Felix | D1strict <68828301+felix-d1strict@users.noreply.github.com>
2020-12-31 01:24:44 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "radio-marzahn",
+  "version": "1.0.0",
+  "description": "alexa-skill for radio-marzahn",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Fehler: no test specified\" && exit 1"
+  },
+  "author": "Sir.Robo Media (Sir.Robo Development) | Radio Marzahn",
+  "license": "none",
+  "dependencies": {
+    "ask-sdk-core": "^2.6.0",
+    "ask-sdk-model": "^1.18.0",
+    "aws-sdk": "^2.326.0"
+  }
+}
